6bcaab86aca59dce800836afba7793cbccae5d76
asharaam1 <lohanaasharaam1@gmail.com>
2024-09-15 16:25:07 +0500

    Js Function, If-Else, Array

---
+++ 1.functions.js
@@ -0,0 +1,21 @@
+//1. Create a block of code that you can use several times. 
+
+
+function greetUser(name){
+    console.log("hello " + name + "!");
+    greeting.innerHTML = "hello " + name + "!"
+    // document.querySelector("#greeting").innerHTML = "hello " + name + "!"
+}
+
+// Call the function multiple times
+greetUser("Abdullah");
+greetUser("Ali");
+greetUser("Saylani");
+
+
+
+// 2. Write a function that displays current date & time in your browser.
+// not studied yet
+
+
+// 3. Write a function that takes first & last name and then it greets the user using his full name.
